7e782b9e82aaf3ba7c43a6cbf342a884d1369521
Kim Dongyeon <rlaehd3459@naver.com>
2024-02-04 14:11:31 +0900

    add console.log for debugging

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -115,6 +115,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems(chosenCategoryShortName["short_name"]);
+      console."ABC";
       console.log(homeHtmlToInsertIntoMainPage);
       homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, 'randomCategoryShortName', chosenCategoryShortName["name"]);
       console.log(chosenCategoryShortName);
